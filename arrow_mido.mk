5380ba07811b96d00071204c1ae32246b61a49cb
Sonal Singh <sonal.singh.19993@gmail.com>
2021-08-28 14:44:45 +0000

    mido: lineage -> arrow
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Change-Id: I31520ccff3fae958978791de45c02dd06a45dee5

--- arrow_mido.mk
+++ arrow_mido.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017 The ArrowOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,12 +21,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+DEVICE_MAINTAINER := Ganesh Varma, Sonal Singh
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := arrow_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
