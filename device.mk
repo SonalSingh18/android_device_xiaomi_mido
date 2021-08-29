6eeb39c7859fa2d2332407e3117945113c0eff8c
Sonal Singh <sonal.singh.19993@gmail.com>
2021-08-29 14:52:04 +0000

    mido: Nuke lineage specific stuffs
     * Lineage Livedisplay HAL
     * Lineage Touch HAL
     * Lineage Trust HAL
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- device.mk
+++ device.mk
@@ -276,10 +276,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_mido
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_mido
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -388,14 +384,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
     android.hardware.thermal@1.0-service
-
-# Touch features
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_mido
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
